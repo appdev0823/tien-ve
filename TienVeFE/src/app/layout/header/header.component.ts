54a340521acfc9fb7e53b1ab7c32900ec789cb52
nguyen.luu <nguyen.luu@flex.com.vn>
2023-10-02 01:21:46 +0700

    feat: update app common UI and header

--- TienVeFE/src/app/layout/header/header.component.ts
+++ TienVeFE/src/app/layout/header/header.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { LoginUserDTO } from 'src/app/dtos';
 import BaseComponent from 'src/app/includes/base.component';
 import { saveAuthStateAction } from 'src/app/store/auth/auth.actions';
+import * as jQuery from 'jquery';
 
 @Component({
     selector: 'app-header',
@@ -10,6 +11,8 @@ import { saveAuthStateAction } from 'src/app/store/auth/auth.actions';
     styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent extends BaseComponent {
+    private _isActionDropdownShowing = false;
+
     /** Constructor */
     constructor(private readonly _router: Router) {
         super();
@@ -20,5 +23,15 @@ export class HeaderComponent extends BaseComponent {
         localStorage.removeItem(this.CONSTANTS.LS_ACCESS_TOKEN_KEY);
         const loginRoute = `/${this.ROUTES.AUTH.MODULE}/${this.ROUTES.AUTH.LOGIN}`;
         await this._router.navigate([loginRoute]);
+    }
+
+    public toggleActionDropdown() {
+        this._isActionDropdownShowing = !this._isActionDropdownShowing;
+
+        if (this._isActionDropdownShowing) {
+            jQuery('#actionDropdown').slideDown(100);
+        } else {
+            jQuery('#actionDropdown').slideUp(100);
+        }
     }
 }
