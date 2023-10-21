ac1945db1f98cc9f94e6590f7e9bd62469b248ab
nguyen.luu <nguyen.luu@flex.com.vn>
2023-10-21 20:39:09 +0700

    source: add stag api URL

--- TienVeFE/src/environments/environment.stag.ts
+++ TienVeFE/src/environments/environment.stag.ts
@@ -1,4 +1,4 @@
 export const environment = {
     mode: 'STAG' as 'DEV' | 'STAG' | 'PROD',
-    api_url: 'https://tien-ve-stag-870680f060fb.herokuapp.com',
+    api_url: 'https://tien-ve-api.miketuannguyen.com',
 };
